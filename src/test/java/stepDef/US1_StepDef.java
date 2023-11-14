a998f975870761635df0a390f75dc002bda0e4d7
kbryz <kbryzga@gmail.com>
2023-11-14 11:19:19 -0500

    US1 commit

---
+++ src/test/java/stepDef/US1_StepDef.java
@@ -0,0 +1,25 @@
+package stepDef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US1_StepDef {
+    @Given("User logged in as {string}")
+    public void user_logged_in_as(String string) {
+
+    }
+    @When("Sending request to API to retrieve all users bio")
+    public void sending_request_to_api_to_retrieve_all_users_bio() {
+
+    }
+    @When("Sending query to DB to get expected users bio")
+    public void sending_query_to_db_to_get_expected_users_bio() {
+
+    }
+    @Then("Verifying match of expected and actual bios")
+    public void verifying_match_of_expected_and_actual_bios() {
+
+    }
+
+}
